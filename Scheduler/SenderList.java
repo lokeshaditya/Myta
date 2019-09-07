90f84ae6b1816d622d5b091829f7ffb9e6959c4f
lokeshaditya <lokeshaditya.3.6.9@gmail.com>
2019-09-07 14:57:19 +0530

    Update SenderList.java

--- Scheduler/SenderList.java
+++ Scheduler/SenderList.java
@@ -15,9 +15,10 @@ import org.quartz.JobExecutionException;
 @SuppressWarnings("serial")
 public class SenderList implements Job{
 	
-		String[] mails = {"lokeshaditya.3.6.9@outlook.com","gmmanohar90@gmail.com","manoharmanit@gmail.com","kancharla68@gmail.com"};
+		//String[] mails = {"lokeshaditya.3.6.9@outlook.com","gmmanohar90@gmail.com","manoharmanit@gmail.com","kancharla68@gmail.com"};
 		public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
+		
 		System.out.println("inside SEnderlist");
 		for(int i=0;i<mails.length-1;i++){
 			
